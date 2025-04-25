e9885a2d28b85046e05b22cc679ca6e08b707dc5
jllloydd <wayfivet@gmail.com>
2025-04-25 20:37:58 +0800

    Added new files, created some text in Hero section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,11 +1,21 @@
-function Hero(){
-    return(
-        <>
-        <section id="hero">
-
-        </section>
-        </>
-    )
+function Hero() {
+  return (
+    <>
+      <section id="hero">
+        <div className="flex flex-col justify-start py-48 px-24 space-y-6">
+        <p className="text-2xl font-semibold">
+            Marci Metzger - The Ridge Reality Group
+        </p>
+        <h1 className="text-5xl font-extrabold">
+            PAHRUMP REALTOR
+        </h1>
+        <button className="px-2 py-2 rounded-lg bg-black text-white w-fit">
+            Call Now
+        </button>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Hero
+export default Hero;
